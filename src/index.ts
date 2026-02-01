309d95bc249e6b52e8317bf368c7c78bc7a760a9
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-01 18:53:12 +0530

    trying to implement oop concepts . started with interface '

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 
 
-console.log("typescript setup working ");
+function sendNotification(type: string, message: string): void {
+
+    if (type === 'console') {
+        console.log(type, 'notifcation' + message)
+    } else if (type === 'email') {
+        console.log(type, 'notifcation' + message)
+    } else if (type === 'sms') {
+        console.log(type, 'notifcation' + message)
+    } else {
+        console.log("unknown notification type ");
+    }
+};
+
+
+
+sendNotification('console',"Build completed");
+sendNotification('email',"you have new message");
+sendNotification('sms',"otp is 1234");
